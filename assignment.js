85705e4dfc833a73cc25d888bafecaa2234ce3b5
1051448tsb <1051448tsb@gmail.com>
2021-04-11 23:42:24 +0800

    first commit

---
+++ assignment.js
@@ -0,0 +1,23 @@
+let country = 'Taiwan';
+let continent = 'Asia';
+let population = '23';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(`i'm ${country}`);
+// template literals
+
+console.log('a' > 'Z');
+// uppercase letters are always “less” than lowercase ones
+
+('use strict');
+
+const x = birthday => x - 100;
+
+console.log('test');
+
+// DEBUG
+
+// HACK
